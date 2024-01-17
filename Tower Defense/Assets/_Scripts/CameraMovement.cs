b94ca73bab0190f2af6b9f09e4242e430dfbffc4
istid1 <mhr97@hotmail.com>
2024-01-17 21:03:24 +0100

    Camera Movement

--- Tower Defense/Assets/_Scripts/CameraMovement.cs
+++ Tower Defense/Assets/_Scripts/CameraMovement.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+
+    private bool _playerInput;
+    [SerializeField] private bool topDownViewActive;
+    [SerializeField] private float cycleLength = 1;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraRotationInput();
     }
+    
+
+    private void CameraRotationInput()
+ {
+    if (_playerInput)
+    {
+        return;
+    }
+
+    if (Input.GetKeyDown(KeyCode.A) && !topDownViewActive)
+    {
+        RotateCamera(new Vector3(0, 90, 0));
+    }
+    else if (Input.GetKeyDown(KeyCode.D) && !topDownViewActive)
+    {
+        RotateCamera(new Vector3(0, -90, 0));
+    }
+    else if (Input.GetKeyDown(KeyCode.W) && !topDownViewActive)
+    {
+        topDownViewActive = true;
+        RotateCamera(new Vector3(45, 0, 0));
+    }
+    else if (Input.GetKeyDown(KeyCode.S) && topDownViewActive)
+    {
+        topDownViewActive = false;
+        RotateCamera(new Vector3(-45, 0, 0));
+    }
+ }
+
+
+    private void RotateCamera(Vector3 rotationAngle)
+    {
+    _playerInput = true;
+    transform.DOLocalRotate(rotationAngle, cycleLength, RotateMode.LocalAxisAdd)
+              .OnComplete(() => _playerInput = false);
+    
+    }
+   
 }
