94cd5854051b3f70d7c5e228bc4745d703d0dd67
istid1 <mhr97@hotmail.com>
2024-03-07 17:12:08 +0100

    fireTower Attack

---
+++ Tower Defense/Assets/_Scripts/FireTowerAttack.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts;
+using UnityEngine;
+using DG.Tweening;
+
+public class FireTowerAttack : MonoBehaviour
+{
+    [SerializeField] private TowerVariables _towerVariables;
+    [SerializeField] private GameObject _fireBulletPrefab;
+    private GameObject _closestEnemy;
+    private float _shortestDistance = Mathf.Infinity;
+    
+    private List<GameObject> _enemies;
+    
+    private float _weaponRange;
+    private float _shootRate;
+    private int _weaponDamage;
+
+    [SerializeField] private GameObject _bulletSpawnPoint;
+ 
+
+    private float _shootTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _weaponRange = _towerVariables.weaponRange;
+        _shootRate = _towerVariables.shootRate;
+        _weaponDamage = _towerVariables.bulletDamage;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FindEnemies();
+        }
+        
+        
+        GameObject closestEnemy = FindClosestEnemy();
+        if (closestEnemy != null)
+        {
+            ShootFireBullet();
+        }
+    }
+
+
+    private GameObject FindClosestEnemy()
+    {
+        _shortestDistance = Mathf.Infinity;
+        foreach (var enemy in _enemies)
+        {
+            if (enemy == null) continue;
+
+            var distanceToEnemy = (enemy.transform.position - gameObject.transform.position).magnitude;
+            if (distanceToEnemy < _shortestDistance)
+            {
+                _shortestDistance = distanceToEnemy;
+                _closestEnemy = enemy;
+            }
+        }
+    
+        return _closestEnemy;
+    }
+
+
+    private void ShootFireBullet()
+    {
+        if (_closestEnemy != null && _shortestDistance <= _weaponRange)
+        {
+            _shootTimer -= Time.deltaTime;
+
+            if (_shootTimer <= 0)
+            {
+                
+                var bullet = Instantiate(_fireBulletPrefab, _bulletSpawnPoint.transform.position, Quaternion.identity);
+                bullet.GetComponent<FireBulletBehavoir>().SetTarget(_closestEnemy.transform);
+                _shootTimer = _shootRate;
+            }
+            
+        }
+    }
+    
+    private void FindEnemies()
+    {
+        _enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+    }
+    
+    
+    
+}
