e83e21fd9aabb7b23e076079c43d19a5cba7024b
istid1 <mhr97@hotmail.com>
2024-03-11 10:51:01 +0100

    yes

--- Tower Defense/Assets/_Scripts/FireBulletBehavoir.cs
+++ Tower Defense/Assets/_Scripts/FireBulletBehavoir.cs
@@ -101,8 +101,6 @@ namespace _Scripts
                     hasHappened = true;
                 }
                 
-                
-                    
             }
         }
         IEnumerator DestroyAfterDelay(float seconds)
