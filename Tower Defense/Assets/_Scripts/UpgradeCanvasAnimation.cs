09bf3956e230d7f6d2a1a751e8d687b19af648e5
Granderous <kevin.b.grande@student.nord.no>
2024-03-11 13:37:31 +0100

    Update UpgradeCanvasAnimation.cs

--- Tower Defense/Assets/_Scripts/UpgradeCanvasAnimation.cs
+++ Tower Defense/Assets/_Scripts/UpgradeCanvasAnimation.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
@@ -14,8 +16,26 @@ namespace _Scripts
         
         [SerializeField] private TowerController _towerController;
 
+
+        private GameObject towerN, damageB, fireRateB, rangeB, xB;
+        
+        
+        private void Start()
+        {
+            towerN = _towerName.gameObject;
+            damageB = _damageButton.gameObject;
+            fireRateB = _fireRateButton.gameObject;
+            rangeB = _rangeButton.gameObject;
+            xB = _xButton.gameObject;
+            
+            UpgradeCanvasDisabled();
+        }
+
+
         public void MoveCanvasActive()
         {
+            UpgradeCanvasActive();
+            
             _towerName.DOAnchorPos(new Vector2(100, 350), 0.5f);
             _damageButton.DOAnchorPos(new Vector2(250, 300), 0.5f);
             _fireRateButton.DOAnchorPos(new Vector2(250, 200), 0.5f);
@@ -26,6 +46,8 @@ namespace _Scripts
         }
         public void MoveCanvasDisabled()
         {
+            UpgradeCanvasDisabled();
+            
             _towerName.DOAnchorPos(new Vector2(700, 0), 0.5f);
             _damageButton.DOAnchorPos(new Vector2(700, 0), 0.5f);
             _fireRateButton.DOAnchorPos(new Vector2(700, 0), 0.5f);
@@ -33,6 +55,33 @@ namespace _Scripts
            _rangeButton.DOAnchorPos(new Vector2(700, 0), 0.5f);
                 
 
+        }
+
+        
+        public void UpgradeCanvasActive()
+        {
+            SetUpgradeCanvasState(true);
+        }
+
+            public void UpgradeCanvasDisabled()
+        { 
+            StartCoroutine(TurnOffCanvas());
+        }
+
+        private void SetUpgradeCanvasState(bool isActive)
+        {
+            towerN.SetActive(isActive);
+            damageB.SetActive(isActive);
+            fireRateB.SetActive(isActive);
+            rangeB.SetActive(isActive);
+            xB.SetActive(isActive);
+        }
+        
+        
+        IEnumerator TurnOffCanvas()
+        {
+            yield return new WaitForSeconds(0.4f);
+            SetUpgradeCanvasState(false);
         }
 
         
