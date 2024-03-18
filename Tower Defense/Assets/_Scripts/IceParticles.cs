cd7e61c1feb82a86c03d56d8ec8bfbbff294d397
istid1 <mhr97@hotmail.com>
2024-03-18 12:21:38 +0100

    iceTower

---
+++ Tower Defense/Assets/_Scripts/IceParticles.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceParticles : MonoBehaviour
+{
+   
+
+    public float _rotationSpeed = -1f;
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0f, 0f, _rotationSpeed);
+    }
+}
