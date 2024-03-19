070208b883c858d6bb231082602f55b6d6fc31b7
istid1 <mhr97@hotmail.com>
2024-03-19 06:22:54 +0100

    iceanimation trigger

--- Tower Defense/Assets/_Scripts/IceParticles.cs
+++ Tower Defense/Assets/_Scripts/IceParticles.cs
@@ -1,22 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class IceParticles : MonoBehaviour
+namespace _Scripts
 {
+    public class IceParticles : MonoBehaviour
+    {
    
 
-    private float _rotationSpeed = -1f;
-    // Start is called before the first frame update
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rotationSpeed = Random.Range(-1f, -2f);
-    }
+        [SerializeField] private TowerVariables _towerVariables;
+        [SerializeField] private IceTowerAnimation _IceTowerAnimation;
+        
+        private int _currentLevelFireRate;
+        
+        private int _rotationSpeed;
+        
+        private int _idleRotationSpeed;
+        
+        
+        private void Start()
+        {
+            _rotationSpeed = Random.Range(-3, 3);
+            if (_rotationSpeed == 0)
+            {
+                _rotationSpeed++;
+            }
+            _idleRotationSpeed = Random.Range(-1, 1);
+            if (_idleRotationSpeed == 0)
+            {
+                _idleRotationSpeed++;
+            }
+                
+        }
     
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.Rotate(0f, 0f, _rotationSpeed);
+        // Update is called once per frame
+        private void Update()
+        {
+
+            _currentLevelFireRate = _towerVariables._currentFireRateUpgradeLevel;
+
+            if (_IceTowerAnimation.isAttacking == false)
+            {
+                transform.Rotate(0f, 0f, _idleRotationSpeed);
+            }
+            
+            if (_IceTowerAnimation.isAttacking)
+            {
+                int rotationSpeed = Math.Abs(_rotationSpeed + _currentLevelFireRate) * -1;
+                if (rotationSpeed == 0)
+                {
+                    rotationSpeed++;
+                }
+                
+                transform.Rotate(0f, 0f, rotationSpeed);
+            }
+           
+            
+                
+          
+            
+        }
     }
 }
--- Tower Defense/Assets/_Scripts/IceTowerAnimation.cs
+++ Tower Defense/Assets/_Scripts/IceTowerAnimation.cs
@@ -9,9 +9,16 @@ namespace _Scripts
         [SerializeField] private TowerVariables _towerVariables;
         [SerializeField] private GameObject _projectile;
 
-
+        public bool isAttacking = false;
+        // The radius of the sphere
+        private float _radius = 3.0f;
+        private bool _enemyInRange = false;
+        
+        // The maximum distance the spherecast will check
+        private float _maxDistance = 0f;
+        
         private int _currentLevel;        
-        private float _startScale = 0.2f;
+        private float _startScale = 0.15f;
         
         // Start is called before the first frame update
         void Start()
@@ -25,7 +32,8 @@ namespace _Scripts
 
             _currentLevel = _towerVariables._currentRangeUpgradeLevel;
             
-            Debug.Log(_currentLevel);
+            RadiusScaleWithRange();
+            SphereCastAndLog();
             
             if (Input.GetKeyDown(KeyCode.P))
             {
@@ -40,26 +48,26 @@ namespace _Scripts
 
         private void UpScale()
         {
-            
+            isAttacking = true;
             switch (_towerVariables._currentRangeUpgradeLevel)
             {
                 case 0:
-                    _projectile.transform.DOScale(new Vector3(_startScale + 0.1f, 0.5f, _startScale + 0.1f), 0.5f);
+                    _projectile.transform.DOScale(new Vector3(_startScale + 0.15f, 0.1f, _startScale + 0.15f), 0.5f);
                     break;
                 case 1:
-                    _projectile.transform.DOScale(new Vector3(_startScale + 0.2f, 0.6f, _startScale + 0.2f), 0.5f);
+                    _projectile.transform.DOScale(new Vector3(_startScale + 0.2f, 0.15f, _startScale + 0.2f), 0.5f);
                     break;
                 case 2:
-                    _projectile.transform.DOScale(new Vector3(_startScale + 0.3f, 0.7f, _startScale + 0.3f), 0.5f);
+                    _projectile.transform.DOScale(new Vector3(_startScale + 0.25f, 0.2f, _startScale + 0.25f), 0.5f);
                     break;
                 case 3:
-                    _projectile.transform.DOScale(new Vector3(_startScale + 0.4f, 0.8f, _startScale + 0.4f), 0.5f);
+                    _projectile.transform.DOScale(new Vector3(_startScale + 0.3f, 0.25f, _startScale + 0.3f), 0.5f);
                     break;
                 case 4:
-                    _projectile.transform.DOScale(new Vector3(_startScale + 0.5f, 0.9f, _startScale + 0.5f), 0.5f);
+                    _projectile.transform.DOScale(new Vector3(_startScale + 0.35f, 0.3f, _startScale + 0.35f), 0.5f);
                     break;
                 case 5:
-                    _projectile.transform.DOScale(new Vector3(_startScale + 0.6f, 1f, _startScale + 0.6f), 0.5f);
+                    _projectile.transform.DOScale(new Vector3(_startScale + 0.375f, 0.325f, _startScale + 0.375f), 0.5f);
                     break;
             }
         }
@@ -67,8 +75,64 @@ namespace _Scripts
 
         private void DownScale()
         {
+            isAttacking = false;
             _projectile.transform.DOScale(new Vector3(_startScale, 0.5f, _startScale), 0.5f);
         }
     
+        
+        
+        
+        private void SphereCastAndLog()
+        {
+            var transform1 = transform;
+            var ray = new Ray(transform1.position, transform1.forward);
+            RaycastHit[] hits;
+
+            var layerMask = 1 << LayerMask.NameToLayer("Enemy");
+
+            
+           
+            hits = Physics.SphereCastAll(ray, _radius, layerMask);
+
+            foreach (var hit in hits)
+            {
+                
+                if(hit.collider.gameObject.CompareTag("Enemy"))
+                {
+                    _enemyInRange = true;
+                   UpScale();
+                   break;
+                }
+                if(!_enemyInRange)
+                {
+                    DownScale();
+                }
+            }
+        }
+        
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            var transform1 = transform;
+            var position = transform1.position;
+            var forward = transform1.forward;
+            Gizmos.DrawRay(position, forward * _maxDistance);
+            Gizmos.DrawWireSphere(position + forward * _maxDistance, _radius);
+        }
+
+        private void RadiusScaleWithRange()
+        {
+            _radius = _currentLevel switch
+            {
+                0 => 5f,
+                1 => 5.5f,
+                2 => 6f,
+                3 => 6.5f,
+                4 => 7f,
+                5 => 7.25f,
+                _ => _radius
+            };
+        }
+        
     }
 }
