a35757208aae226dabc4fa5c59b9c3ae3d7519ee
istid1 <mhr97@hotmail.com>
2024-04-11 17:07:55 +0200

    ad skip button

--- Tower Defense/Assets/_Scripts/Ads.cs
+++ Tower Defense/Assets/_Scripts/Ads.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEditor.UI;
+using UnityEngine.UI;
 
 namespace _Scripts
 {
@@ -12,22 +14,33 @@ namespace _Scripts
 
         [SerializeField] private GameObject _canvas1;
         [SerializeField] private GameObject _canvas2;
+        [SerializeField] private GameObject _skipButtonCanvas;
+
+        [SerializeField] private GameObject _exitAdButton;
 
         private bool _hasPlayed;
         public float _timeRemaining = 30;
+
+        
+        public float totalAdTime = 30;
+        private bool _countDownComplete;
+        
+        [SerializeField] private Image countdownCircle;
         
         // Start is called before the first frame update
         void Start()
         {
-            
+            countdownCircle = countdownCircle.GetComponent<Image>();
+            countdownCircle.fillAmount = 1;
         }
 
         // Update is called once per frame
         void Update()
         {
 
-            if ( _gameManager.currentRound == 5 && !_hasPlayed)
+            if ( _gameManager.currentRound == 1 && !_hasPlayed)
             {
+                _skipButtonCanvas.SetActive(true);
                 _videoPlayer.Play();
                 _hasPlayed = true;
                 Time.timeScale = 0;
@@ -47,15 +60,35 @@ namespace _Scripts
             {
                 yield return new WaitForSecondsRealtime(1);
                 _timeRemaining--;
+                countdownCircle.fillAmount = _timeRemaining / totalAdTime;
+                if (countdownCircle.fillAmount == 0)
+                {
+                    
+                    countdownCircle.enabled = false;
+                    _exitAdButton.SetActive(true);
+                }
             }
+
+            _countDownComplete = true;
+           
+           
+        }
+
+        public void ExitAdButton()
+        {
+           ReturnToGame();
+        }
+
+        private void ReturnToGame()
+        {
             _videoPlayer.Stop();
             Time.timeScale = 1;
             _canvas1.SetActive(true);
             _canvas2.SetActive(true);
-            Debug.Log("DOOOOONEEEEEEE");
+            _skipButtonCanvas.SetActive(false);
         }
         
         
-        
+
     }
 }
