0be918ec96042952b038bb3f24926b2d86de8b38
istid1 <mhr97@hotmail.com>
2024-04-19 15:38:30 +0200

    aseg

--- Tower Defense/Assets/_Scripts/Ads.cs
+++ Tower Defense/Assets/_Scripts/Ads.cs
@@ -20,7 +20,8 @@ namespace _Scripts
 
         public bool _hasPlayed;
         public float _timeRemaining = 30;
-
+        private int _previousRound = -1;
+        
         
         public float totalAdTime = 30;
         private bool _countDownComplete;
@@ -37,24 +38,32 @@ namespace _Scripts
         // Update is called once per frame
         void Update()
         {
-
-            if ( _gameManager.currentRound % 2 == 0 && !_hasPlayed)
+            
+            if (_gameManager.currentRound % 2 == 0 && !_hasPlayed)   //Dissable canvases and start playing ads on round 2,4,6,8,etc... (not on round 0)
             {
-                _skipButtonCanvas.SetActive(true);
-                _videoPlayer.Play();
-                _hasPlayed = true;
-                Time.timeScale = 0;
-                _canvas1.SetActive(false);
-                _canvas2.SetActive(false);
-                StartCoroutine(StartCountdown());
+                if (_gameManager.currentRound > 0)
+                {
+                    _skipButtonCanvas.SetActive(true);
+                    _videoPlayer.Play();
+                    _hasPlayed = true;
+                    Time.timeScale = 0;
+                    _canvas1.SetActive(false);
+                    _canvas2.SetActive(false);
+                    StartCoroutine(StartCountdown());
+                }
+                    
             }
-           
             
+
             
+            if (!_videoPlayer.isPlaying && _countDownComplete) // send the player back to the game if the video player is done playing and the skip button wasn't pressed.
+            {
+                ReturnToGame();
+            }
             
         }
         
-        private IEnumerator StartCountdown()
+        private IEnumerator StartCountdown() //Countdown and show the player when they can press the skip ad button.
         {
             while (_timeRemaining > 0)
             {
@@ -68,25 +77,25 @@ namespace _Scripts
                     _exitAdButton.SetActive(true);
                 }
             }
-
             _countDownComplete = true;
-           
-           
         }
+        
 
         public void ExitAdButton()
         {
            ReturnToGame();
         }
 
-        private void ReturnToGame()
+        private void ReturnToGame() //speaks for itself
         {
+            _hasPlayed = false;
             _videoPlayer.Stop();
             Time.timeScale = 1;
             
             _canvas1.SetActive(true);
             _canvas2.SetActive(true);
             _skipButtonCanvas.SetActive(false);
+            
         }
         
         
